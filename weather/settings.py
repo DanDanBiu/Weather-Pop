8fed07682fa711f9d0a46d21a303c0cd69ec90a4
dandanbiu <dandanbiubiu@gmail.com>
2017-01-05 19:28:01 -0800

    Render front-end with Bootstrap

--- weather/settings.py
+++ weather/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
-import os
 import json
+import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'reminder',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
